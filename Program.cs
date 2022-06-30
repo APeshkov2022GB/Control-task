1a123b4c54a1a27237ece20743733b28a0b12337
Aleksandr Peshkov <pas-0787@mail.ru>
2022-06-30 23:59:44 +0300

    Change 1 in Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива");
+int n = int.Parse(Console.ReadLine()?? "");
+string[] array1 = new string[n];
+InputArray(array1);
+PrintArray(array1);
+string[] array2 = new string[array1.Length];
+ArraySort(array1, array2);
+PrintArray(array2);
+Console.WriteLine("Программа работу закончила");
